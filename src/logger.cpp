7307b3f8c1d819b42fb79ae67623586328687c2d
Mariola Kisielewska <mario.kisielewska@gmail.com>
2022-03-13 12:15:18 +0100

    switch

--- src/logger.cpp
+++ src/logger.cpp
@@ -13,41 +13,74 @@ public:
     };
 
 public:
-    static void logInfo(const std::string& msg)
+
+    static void log(const std::string& msg, Logger::Level level)
     {
-        if (logLevel >= Level::INFO)
+        switch(level)
         {
-            std::cout << "\033[1;37m"
+            case Level::INFO:
+                std::cout << "\033[1;37m"
                 << "[Info] "
                 << msg
                 << "\033[0m"
                 << std::endl;
-        }
-    }
-
-    static void logWarning(const std::string& msg)
-    {
-        if (logLevel >= Level::WARNING)
-        {
-            std::cout << "\033[1;33m"
-                << "[Warning] "
+                break;
+        
+            case Level::WARNING:
+                std::cout << "\033[1;33m"
+                << "[Info] "
                 << msg
                 << "\033[0m"
                 << std::endl;
-        }
-    }
-
-    static void logError(const std::string& msg)
-    {
-        if (logLevel >= Level::ERROR)
-        {
-            std::cout << "\033[1;31m"
-                << "[Error] "
+                break;
+            
+            case Level::ERROR:
+                std::cout << "\033[1;31m"
+                << "[Info] "
                 << msg
                 << "\033[0m"
                 << std::endl;
+                break;
+            default:
+                break;
         }
+
     }
+    // static void logInfo(const std::string& msg)
+    // {
+    //     if (logLevel >= Level::INFO)
+    //     {
+    //         std::cout << "\033[1;37m"
+    //             << "[Info] "
+    //             << msg
+    //             << "\033[0m"
+    //             << std::endl;
+    //     }
+    // }
+
+    // static void logWarning(const std::string& msg)
+    // {
+    //     if (logLevel >= Level::WARNING)
+    //     {
+    //         std::cout << "\033[1;33m"
+    //             << "[Warning] "
+    //             << msg
+    //             << "\033[0m"
+    //             << std::endl;
+    //     }
+    // }
+
+    // static void logError(const std::string& msg)
+    // {
+    //     if (logLevel >= Level::ERROR)
+    //     {
+    //         std::cout << "\033[1;31m"
+    //             << "[Error] "
+    //             << msg
+    //             << "\033[0m"
+    //             << std::endl;
+    //     }
+    // }
 
     static void setLevel(Level level)
     {
@@ -65,9 +98,12 @@ int main()
     Logger::setLevel(Logger::Level::ERROR);
     //Logger::setLevel(Logger::Level::WARNING);
     //Logger::setLevel(Logger::Level::INFO);
-    Logger::logInfo("info");
-    Logger::logWarning("warning");
-    Logger::logError("error");
+    // Logger::logInfo("info");
+    // Logger::logWarning("warning");
+    // Logger::logError("error");
+    Logger::log("cokolwiek", Logger::Level::ERROR);
+
+
 
     return 0;
 }
