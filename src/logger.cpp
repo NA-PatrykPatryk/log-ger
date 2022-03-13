11632b3a71be45a9a184583b0d8c053132ccf977
Daniel Konoplański <daniel_konoplanski2@int.pl>
2022-03-13 11:47:59 +0100

    niewiem

--- src/logger.cpp
+++ src/logger.cpp
@@ -13,39 +13,51 @@ public:
     };
 
 public:
-    static void logInfo(const std::string& msg)
-    {
-        if (logLevel >= Level::INFO)
-        {
-            std::cout << "\033[1;37m"
-                << "[Info] "
-                << msg
-                << "\033[0m"
-                << std::endl;
-        }
-    }
+    // static void logInfo(const std::string& msg)
+    // {
+    //     if (logLevel >= Level::INFO)
+    //     {
+    //         std::cout << "\033[1;37m"
+    //             << "[Info] "
+    //             << msg
+    //             << "\033[0m"
+    //             << std::endl;
+    //     }
+    // }
 
-    static void logWarning(const std::string& msg)
-    {
-        if (logLevel >= Level::WARNING)
-        {
-            std::cout << "\033[1;33m"
-                << "[Warning] "
-                << msg
-                << "\033[0m"
-                << std::endl;
-        }
-    }
+    // static void logWarning(const std::string& msg)
+    // {
+    //     if (logLevel >= Level::WARNING)
+    //     {
+    //         std::cout << "\033[1;33m"
+    //             << "[Warning] "
+    //             << msg
+    //             << "\033[0m"
+    //             << std::endl;
+    //     }
+    // }
 
-    static void logError(const std::string& msg)
+    // static void logError(const std::string& msg)
+    // {
+    //     if (logLevel >= Level::ERROR)
+    //     {
+    //         std::cout << "\033[1;31m"
+    //             << "[Error] "
+    //             << msg
+    //             << "\033[0m"
+    //             << std::endl;
+    //     }
+    // }
+
+    static void log(const std::string& msg, Level level)
     {
-        if (logLevel >= Level::ERROR)
+        switch (level)
         {
-            std::cout << "\033[1;31m"
-                << "[Error] "
-                << msg
-                << "\033[0m"
-                << std::endl;
+        case Level::INFO:
+            break;
+        
+        default:
+            break;
         }
     }
 
