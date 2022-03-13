65fe6f10641213c8c4b4e52107a21c3ce7ee0cfb
Patryk Szlachetka <patryk.szlachetka95@gmail.com>
2022-03-13 11:09:45 +0100

    added barebones logger class

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::cout << "it is working, as intended" << std::endl;
+class Logger
+{
+public:
+    static void log(const std::string& msg)
+    {
+        std::cout << msg << std::endl;
+    }
+
+private:
+
+};
+
+int main() 
+{
+    Logger::log("it is working, as intended");
 
     return 0;
 }
