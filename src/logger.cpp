08944104d8885c2e429af6f62fe57b111f562e9f
Daniel Konoplański <daniel_konoplanski2@int.pl>
2022-03-13 11:44:27 +0100

    Basic Logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -4,21 +4,70 @@
 class Logger
 {
 public:
-    enum class Level{INFO, WARNING, ERROR};
+    enum class Level
+    {
+        ERROR,
+        WARNING,
+        INFO,
+        max_level
+    };
 
 public:
-    static void log(const std::string& msg)
+    static void logInfo(const std::string& msg)
     {
-        std::cout << msg << std::endl;
+        if (logLevel >= Level::INFO)
+        {
+            std::cout << "\033[1;37m"
+                << "[Info] "
+                << msg
+                << "\033[0m"
+                << std::endl;
+        }
+    }
+
+    static void logWarning(const std::string& msg)
+    {
+        if (logLevel >= Level::WARNING)
+        {
+            std::cout << "\033[1;33m"
+                << "[Warning] "
+                << msg
+                << "\033[0m"
+                << std::endl;
+        }
+    }
+
+    static void logError(const std::string& msg)
+    {
+        if (logLevel >= Level::ERROR)
+        {
+            std::cout << "\033[1;31m"
+                << "[Error] "
+                << msg
+                << "\033[0m"
+                << std::endl;
+        }
+    }
+
+    static void setLevel(Level level)
+    {
+        logLevel = level;
     }
 
 private:
-    Level logLevel = Level::INFO;
+    static Level logLevel;
 };
 
-int main() 
+Logger::Level Logger::logLevel = Logger::Level::INFO;
+
+int main()
 {
-    Logger::log("it is working, as intended");
+    Logger::setLevel(Logger::Level::ERROR);
+    //Logger::setLevel(Logger::Level::WARNING);
+    //Logger::setLevel(Logger::Level::INFO);
+    Logger::logInfo("info");
+    Logger::logWarning("warning");
+    Logger::logError("error");
 
     return 0;
 }
